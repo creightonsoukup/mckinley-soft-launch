090f6c9c3d81b06ca5667156d3a380be835aa7c9
Your Name <creighton@creightonsoukup.com>
2016-12-06 11:38:40 -0700

    fixed error

--- public/partials/home/home.controller.js
+++ public/partials/home/home.controller.js
@@ -3,15 +3,15 @@
   .controller('Home', function ($scope, $location, navigationService, $http) {
     $scope.view = {}
     $scope.view.quote = {}
-    $http.get({
-      url: 'http://quotes.rest/quote?minlength=100&maxlength=300',
-      method: 'get',
-      headers: {
-        'Accept': 'application/json',
-        'X-TheySaidSo-Api-Secret': 'P2LzVd34DJjYaM9iMBk6JweF'
-      }
-    })
-      .then((data) => console.log(data))
+    // $http.get({
+    //   url: 'http://quotes.rest/quote?minlength=100&maxlength=300',
+    //   method: 'get',
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'X-TheySaidSo-Api-Secret': 'P2LzVd34DJjYaM9iMBk6JweF'
+    //   }
+    // })
+      // .then((data) => console.log(data))
     $scope.view.toggle = ""
     $scope.view.toggleClass = () => {
       if ($scope.view.toggle === "") {

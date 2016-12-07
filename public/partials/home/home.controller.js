7ca0fa6fb8ee32f097992e6004f7478199694af2
Your Name <creighton@creightonsoukup.com>
2016-12-06 21:06:53 -0700

    some fixes

--- public/partials/home/home.controller.js
+++ public/partials/home/home.controller.js
@@ -3,15 +3,19 @@
   .controller('Home', function ($scope, $location, navigationService, $http) {
     $scope.view = {}
     $scope.view.quote = {}
-    // $http.get({
-    //   url: 'http://quotes.rest/quote?minlength=100&maxlength=300',
-    //   method: 'get',
-    //   headers: {
-    //     'Accept': 'application/json',
-    //     'X-TheySaidSo-Api-Secret': 'P2LzVd34DJjYaM9iMBk6JweF'
-    //   }
-    // })
-      // .then((data) => console.log(data))
+    $http({
+      url: 'http://quotes.rest/quote?minlength=100&maxlength=300',
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'X-TheySaidSo-Api-Secret': 'P2LzVd34DJjYaM9iMBk6JweF'
+      }
+    }).then((data) => {
+      console.log(data.data.contents)
+      $scope.view.quote.text = data.data.contents.quote
+      $scope.view.quote.author = data.data.contents.author
+      console.log($scope.view.quote.text, $scope.view.quote.author)
+    })
     $scope.view.toggle = ""
     $scope.view.toggleClass = () => {
       if ($scope.view.toggle === "") {

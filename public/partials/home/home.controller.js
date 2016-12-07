bd4f956212152b510b0afb0ab808174d1ae39fc1
Your Name <creighton@creightonsoukup.com>
2016-12-06 21:22:57 -0700

    scope apply added

--- public/partials/home/home.controller.js
+++ public/partials/home/home.controller.js
@@ -14,7 +14,7 @@
       console.log(data.data.contents)
       $scope.view.quote.text = data.data.contents.quote
       $scope.view.quote.author = data.data.contents.author
-      console.log($scope.view.quote.text, $scope.view.quote.author)
+      $scope.$apply()
     })
     $scope.view.toggle = ""
     $scope.view.toggleClass = () => {

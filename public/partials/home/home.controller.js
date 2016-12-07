2425a1f498d5957a92e63c3bf6c8ac67cebc0646
Your Name <creighton@creightonsoukup.com>
2016-12-06 21:47:35 -0700

    horseshoe

--- public/partials/home/home.controller.js
+++ public/partials/home/home.controller.js
@@ -4,7 +4,7 @@
     $scope.view = {}
     $scope.view.quote = {}
     $http({
-      url: 'https://quotes.rest/quote?minlength=100&maxlength=300',
+      url: 'http://quotes.rest/quote?minlength=100&maxlength=300',
       method: 'GET',
       headers: {
         'Accept': 'application/json',
@@ -14,7 +14,6 @@
       console.log(data.data.contents)
       $scope.view.quote.text = data.data.contents.quote
       $scope.view.quote.author = data.data.contents.author
-      $scope.$apply()
     })
     $scope.view.toggle = ""
     $scope.view.toggleClass = () => {
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,14 @@ var router = express.Router();
 var ShopifyBuy = require('shopify-buy');
 
 router.get('/quote', function(req,res,next) {
-  http.get('http://quotes.rest/quote?minlength=100&maxlength=300')
-    .then
+  http.get({
+    url: 'http://quotes.rest/quote?minlength=100&maxlength=300',
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'X-TheySaidSo-Api-Secret': 'P2LzVd34DJjYaM9iMBk6JweF'
+    }
+  }).then((data) => res.send(data))
 })
 
 
